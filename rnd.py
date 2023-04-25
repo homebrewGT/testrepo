540e29b429b51239e803b7696ebffdc6ddcf1f1f
homebrewGT <davisrayler@hotmail.com>
2023-04-24 21:46:10 -0400

    Just testing

--- rnd.py
+++ rnd.py
@@ -1,8 +1,9 @@
-def print_lyrics():
-    print("I'm a lumberjack, and I'm okay.")
-    print("I sleep all nightn and I work all day.")
 def repeat_lyrics():
     print_lyrics()
     print_lyrics()
+    
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
 
 repeat_lyrics()
